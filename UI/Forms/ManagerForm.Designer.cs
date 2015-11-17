b9180d8e2c3a03d182c381ea54bc4bb5f0147f6d
AliaksandrNovik <na49526@yandex.ru>
2015-11-17 22:03:57 +0300

    added russian names

--- UI/Forms/ManagerForm.Designer.cs
+++ UI/Forms/ManagerForm.Designer.cs
@@ -131,7 +131,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(138, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Найти";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
@@ -143,7 +143,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(138, 23);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "Найти";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // dataGridView2
@@ -179,7 +179,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(138, 23);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
+            this.button3.Text = "Найти";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
@@ -200,7 +200,7 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 5;
-            this.button4.Text = "button4";
+            this.button4.Text = "Добавить";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
@@ -209,7 +209,7 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 6;
-            this.button5.Text = "button5";
+            this.button5.Text = "Изменить";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
@@ -218,7 +218,7 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 23);
             this.button6.TabIndex = 7;
-            this.button6.Text = "button6";
+            this.button6.Text = "Удалить";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // tabPage1
@@ -243,7 +243,7 @@
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(75, 23);
             this.button7.TabIndex = 4;
-            this.button7.Text = "button7";
+            this.button7.Text = "Добавить";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // button8
@@ -252,7 +252,7 @@
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(75, 23);
             this.button8.TabIndex = 5;
-            this.button8.Text = "button8";
+            this.button8.Text = "Изменить";
             this.button8.UseVisualStyleBackColor = true;
             // 
             // button9
@@ -261,7 +261,7 @@
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(75, 23);
             this.button9.TabIndex = 6;
-            this.button9.Text = "button9";
+            this.button9.Text = "Удалить";
             this.button9.UseVisualStyleBackColor = true;
             // 
             // button10
@@ -270,7 +270,7 @@
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(75, 23);
             this.button10.TabIndex = 3;
-            this.button10.Text = "button10";
+            this.button10.Text = "Удалить";
             this.button10.UseVisualStyleBackColor = true;
             // 
             // button11
@@ -279,7 +279,7 @@
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(75, 23);
             this.button11.TabIndex = 4;
-            this.button11.Text = "button11";
+            this.button11.Text = "Добавить";
             this.button11.UseVisualStyleBackColor = true;
             // 
             // button12
@@ -288,7 +288,7 @@
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(75, 23);
             this.button12.TabIndex = 5;
-            this.button12.Text = "button12";
+            this.button12.Text = "Изменить";
             this.button12.UseVisualStyleBackColor = true;
             // 
             // ManagerForm
